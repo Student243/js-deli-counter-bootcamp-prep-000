d24351cfb1d5bea5fd52fcff564a4a09574b79aa
Kevin Andrew Johnson <kjohnson5315@hotmail.com>
2018-06-15 12:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return `The line is currently empty.`
-  } if (katzDeliLine.length > 0) {
+  } 
    return 
   }
-}
